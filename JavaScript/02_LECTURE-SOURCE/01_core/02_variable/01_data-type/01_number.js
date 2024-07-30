2c4dfe15fca0d5a3d7ef6e6f97338a4752298f94
JungHyeonmin <2ee42a45@gmail.com>
2024-07-30 23:23:15 +0900

    javaScript 3

--- JavaScript/02_LECTURE-SOURCE/01_core/02_variable/01_data-type/01_number.js
+++ JavaScript/02_LECTURE-SOURCE/01_core/02_variable/01_data-type/01_number.js
@@ -2,3 +2,15 @@ var integer = 10;
 
 console.log(integer );
 console.log(typeof integer);
+
+test =1;
+console.log(typeof test);
+
+test = ture;
+console.log(typeof test);
+
+test = null;
+console.log(typeof test);
+
+test = Symbol();
+console.log(typeof test);
