fd58938b464c47e2514c14a5fb0d9d5e865ded80
JungHyeonmin <2ee42a45@gmail.com>
2024-08-10 14:37:44 +0900

    8/10 (토) JavaScript 11일차

--- JavaScript/02_LECTURE-SOURCE/01_core/10_strict-mode/02_example/01_error-case.js
+++ JavaScript/02_LECTURE-SOURCE/01_core/10_strict-mode/02_example/01_error-case.js
@@ -1,7 +1,9 @@
-/* 01_error-case(에러 상황) */
+/** 01_error-case(에러 상황)
+ *
+ */
 
 // 1. 암묵적 전역
-(function(){
+(function () {
     // 'use strict';
 
     // 선언하지 않은 변수를 참조하면 에러가 발생한다.
@@ -10,7 +12,7 @@
 }());
 
 // 2. 변수, 함수, 매개변수의 삭제
-(function(){
+(function () {
     // 'use strict';
 
     var x = 1;
@@ -19,7 +21,7 @@
 }());
 
 // 3. 매개변수 이름의 중복
-(function(){
+(function () {
     //'use strict';
 
     function test(x, x) {   // SyntaxError: Duplicate parameter name not allowed in this context
@@ -30,13 +32,13 @@
 }());
 
 // 4. with 문의 사용
-(function(){
+(function () {
     //'use strict';
 
     // with문은 전달된 객체를 스코프 체인에 추가한다.
     // 객체 이름을 생략할 수 있어 코드가 간단해지느 효과가 있지만 
     // 성능과 가독성이 나빠지는 문제가 있어 사용하지 않는 것이 좋다.
-    with({ x : 1 }) {   // 'with' 문은 strict 모드에서 사용할 수 없습니다.
+    with ({x: 1}) {   // 'with' 문은 strict 모드에서 사용할 수 없습니다.
         console.log(x);
     }
 }());
