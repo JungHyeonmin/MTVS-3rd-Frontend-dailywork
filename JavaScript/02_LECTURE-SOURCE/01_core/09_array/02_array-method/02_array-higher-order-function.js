0bd71889e0b843e41069ca3982605ab9f6609fca
JungHyeonmin <2ee42a45@gmail.com>
2024-08-10 22:37:17 +0900

    8/10 (토) JavaScript 13일차

--- JavaScript/02_LECTURE-SOURCE/01_core/09_array/02_array-method/02_array-higher-order-function.js
+++ JavaScript/02_LECTURE-SOURCE/01_core/09_array/02_array-method/02_array-higher-order-function.js
@@ -1,11 +1,47 @@
-// 배열 고차 함수
-// * 고차 함수 : 함수를 인수로 전달 받거나 함수를 반환하는 함수
-
+/**
+ * 9-2. 배열 메서드
+ *
+ * 9-2-2. 배열 고차 함수
+ * 고차 함수 : 함수를 인수로 전달 받거나 함수를 반환하는 함수
+ *
+ * Array.prototype.sort
+ * - 배열을 정렬 기준으로 정렬한다.
+ *
+ * - 오름차순 정렬이 기본이면 정렬 후 정렬 순서를 유지한다.
+ * - 배열은 기본적으로 문자열 정럴이 되므로 일부 올바르지 않게 정렬 되는 모습을 확인할 수 있다.
+ * - 다른 정렬 기준을 사용하려면 매개변수로 compare 함수를 전달해야 한다.
+ *
+ * Array.prototype.forEach
+ * - for 를 대체할 수 있는 고차함수이다.
+ *
+ * Array.prototype.map
+ * - 배열 요소 전체를 대상으로 콜백 함수 호출 후 반환 값들로 구성 된 새로운 배열을 반환한다.
+ *
+ * Array.prototype.filter
+ * - 배열 요소 전체를 대상으로 콜백 함수 호출 후 반환 값이 true 인 요소로만 구성된 새로운 배열 반환한다.
+ *
+ * Array.prototype.reduce
+ * - 배열을 순회하며 각 요소에 대하여 이전 콜백함수 실행 반환값을 전달하여 콜백함수를 실행하고 그 결과를 반환한다.
+ *
+ * Array.prototype.some
+ * - 배열 내 일부 요소가 콜백 함수의 테스트를 통과하는지 확인하여 그 결과를 boolean 으로 반환한다.
+ *
+ * Array.prototype.every
+ * - 배열 내 모든 요소가 콜백함수의 테스트를 통과하는지 확인하여 그 결과를 boolean 으로 반환한다.
+ *
+ * Array.prototype.find, Array.prototype.findIndex
+ * - find : 배열을 순회하먀 각 요소에 대하여 인자로 주어진 콜백함수를 실행하여 그 결과가 참인 첫번째 요소를 반환
+ *      - 참인 요소가 존재하지 않는다면 첫번째 요소를 반환
+ * - findIndex : 배열을 순회하며 각 요소에 대하여 인자로 주어진 콜백함수를 실행하여 그 결과가 참인 첫번째 요소의 인덱스를 반환
+ *      - 참인 요소가 존재하지 않는다면 '-1'을 반환
+ */
 // Array.prototype.sort : 배열을 정렬 기준으로 정렬
 let numbers = [];
+let numbers2 = [];
 
 for (let i = 0; i < 10; i++) {
     numbers[i] = Math.floor(Math.random() * 100) + 1;
+    numbers2[i] = Math.floor(Math.random() * 100) + 1;
 }
 
 console.log(`정렬 전 numbers : ${numbers}`);
@@ -15,34 +51,37 @@ console.log(`정렬 전 numbers : ${numbers}`);
 // => 다른 정렬 기준을 사용하려면 매개변수로 compare 함수 전달
 numbers.sort();
 console.log(`정렬 후 numbers : ${numbers}`);
-
+// 자신이 원하는 방식으로 정렬하는 compare 함수
 // 숫자 오름차순 정렬
 function compare(a, b) {
-    if(a > b) return 1;
-    if(a == b) return 0;
-    if(a < b) return -1;
+    if (a > b) return 1;  // 양수 : 오름차순
+    if (a == b) return 0; // 0 : 변경없음
+    if (a < b) return -1; // 음수 : 내림차순
 }
 
-numbers.sort(compare);
+numbers2.sort(compare);
 console.log('매개변수로 compare 함수 전달하여 숫자 오름차순 정렬');
-console.log(numbers);
+console.log(`compare 정렬 전 numbers2 : ${numbers2}`);
+console.log(numbers2);
 
 // 숫자 내림차순 정렬
 // numbers.number(function(a, b){
 //     return b - a;
 // });
-numbers.sort((a, b) => b - a);
+numbers2.sort((a, b) => b - a);
 
-console.log('숫자 내림차순 정렬');
-console.log(numbers);
+console.log('numbers2 숫자 내림차순 정렬');
+console.log(numbers2);
 
 // Array.prototype.forEach : for를 대체할 수 있는 고차함수
 
-/* 참고
-함수형 프로그래밍 : 순수 함수와 보조 함수의 조합을 통해 로직 내에 존재하는 조건문과 반복문을 제거하여
-복잡성을 해결하고 변수의 사용을 억제하여 상태 변경을 피하려는 프로그래밍 패러다임
-for문은 반복을 위한 변수 선언, 조건식, 증감식 등이 필요하여 함수형 프로그램이 추구하는 바와 맞지 않음
-forEach 메서드는 for를 대체할 수 있는 고차함수 */
+/**
+ * 참고
+ * 함수형 프로그래밍 : 순수 함수와 보조 함수의 조합을 통해 로직 내에 존재하는 조건문과 반복문을 제거하여
+ * 복잡성을 해결하고 변수의 사용을 억제하여 상태 변경을 피하려는 프로그래밍 패러다임
+ * for문은 반복을 위한 변수 선언, 조건식, 증감식 등이 필요하여 함수형 프로그램이 추구하는 바와 맞지 않음
+ * forEach 메서드는 for를 대체할 수 있는 고차함수
+ */
 
 /*
 배열.forEach(function(item, index, array){
@@ -51,7 +90,7 @@ forEach 메서드는 for를 대체할 수 있는 고차함수 */
 */
 numbers = [1, 2, 3, 4, 5];
 
-numbers.forEach(function(item, index, array){
+numbers.forEach(function (item, index, array) {
     console.log(`item : ${item}`);
     console.log(`index : ${index}`);
     console.log(`array : ${array}`);
@@ -84,7 +123,7 @@ currentValue : 배열 요소의 값
 currentIndex : 인덱스
 array        : 메소드를 호출한 배열, 즉 this
 */
-numbers.reduce(function(previousValue, currentValue, currentIndex, array){
+numbers.reduce(function (previousValue, currentValue, currentIndex, array) {
     console.log(`previousValue : ${previousValue}`);
     console.log(`currentValue : ${currentValue}`);
     console.log(`currentIndex : ${currentIndex}`);
@@ -93,36 +132,36 @@ numbers.reduce(function(previousValue, currentValue, currentIndex, array){
 
 // 합산
 const sum = numbers.reduce(function (previousValue, currentValue) {
-  return previousValue + currentValue; // 결과는 다음 콜백의 첫번째 인자로 전달된다
+    return previousValue + currentValue; // 결과는 다음 콜백의 첫번째 인자로 전달된다
 });
 console.log(`sum : ${sum}`); // 15: 1~5까지의 합
 
 // 최대값 취득
 const max = numbers.reduce(function (pre, cur) {
-  return pre > cur ? pre : cur;
+    return pre > cur ? pre : cur;
 });
 console.log(`max : ${max}`); // 5: 최대값
 
 // Array.prototype.some : 배열 내 일부 요소가 콜백 함수의 테스트를 통과하는지 확인하여 그 결과를 boolean으로 반환
 
 // 배열 내 요소 중 10보다 큰 값이 1개 이상 존재하는지 확인
-let result = [1, 5, 3, 2, 4].some(item  => item > 10);
+let result = [1, 5, 3, 2, 4].some(item => item > 10);
 console.log(`result : ${result}`); // false
 // 배열 내 요소 중 3보다 큰 값이 1개 이상 존재하는지 확인  
-result = [1, 5, 3, 2, 4].some(item  => item > 3);
+result = [1, 5, 3, 2, 4].some(item => item > 3);
 console.log(`result : ${result}`);  // true
-  
+
 // 배열 내 요소 중 특정 값이 1개 이상 존재하는지 확인
-result = ['apple', 'banana', 'cat', 'dog'].some(item  => item === 'egg');
+result = ['apple', 'banana', 'cat', 'dog'].some(item => item === 'egg');
 console.log(`result : ${result}`); // false
-result = ['apple', 'banana', 'cat', 'dog'].some(item  => item === 'dog');
+result = ['apple', 'banana', 'cat', 'dog'].some(item => item === 'dog');
 console.log(`result : ${result}`);  // true
 
 // Array.prototype.every : 배열 내 모든 요소가 콜백함수의 테스트를 통과하는지 확인하여 그 결과를 boolean으로 반환
-result = [1, 5, 3, 2, 4].every(item  => item > 3);
+result = [1, 5, 3, 2, 4].every(item => item > 3);
 console.log(`result : ${result}`); // false
 // 배열 내 요소 중 3보다 큰 값이 1개 이상 존재하는지 확인  
-result = [1, 5, 3, 2, 4].every(item  => item > 0);
+result = [1, 5, 3, 2, 4].every(item => item > 0);
 console.log(`result : ${result}`);  // true
 
 // Array.prototype.find : 배열을 순회하며 각 요소에 대하여 인자로 주어진 콜백함수를 실행하여 그 결과가 참인 첫번째 요소를 반환
@@ -130,9 +169,9 @@ console.log(`result : ${result}`);  // true
 // Array.prototype.findIndex : 배열을 순회하며 각 요소에 대하여 인자로 주어진 콜백함수를 실행하여 그 결과가 참인 첫번째 요소의 인덱스를 반환
 // 참인 요소가 존재하지 않는다면 -1을 반환
 const students = [
-    { name : '유관순', score : 90 },
-    { name : '홍길동', score : 80 },
-    { name : '장보고', score : 70 }
+    {name: '유관순', score: 90},
+    {name: '홍길동', score: 80},
+    {name: '장보고', score: 70}
 ];
 result = students.find(item => item.name === '유관순');
 console.log(result);    // { name: '유관순', score: 90 }
